22d82ed6beac7388c99bf6b33d88286804746133
Andrew Dansby <andydansby@gmail.com>
2020-10-19 14:20:02 -0400

    Add files via upload

---
+++ globalVariables.h
@@ -0,0 +1,56 @@
+
+// Array of enemy locations in the game
+// We use the same Entity structure, with just one difference: X now means the tile
+
+struct Enemy
+{
+	unsigned char x;        // Tile in X
+	unsigned char x_desp;   // Displacement in tile (0-23)
+	unsigned char y;        // Y position
+	unsigned char sprnum;	// Number of the enemy sprite: if 0, this enemy is not active
+	unsigned char movement;	// movement type, from one of the predefined ones
+	unsigned char energy;
+	unsigned char param1;
+	unsigned char param2;	// Two parameters, to store some information that is entity-specific
+} Enemy_structure[MAX_ENEMIES];
+
+struct baddies
+{
+	unsigned char x;        // Tile in X
+	unsigned char x_desp;   // Displacement in tile (0-23)
+	unsigned char y;        // Y position
+	unsigned char sprnum;	// Number of the enemy sprite: if 0, this enemy is not active
+	unsigned char movement;	// movement type, from one of the predefined ones
+	unsigned char energy;
+	unsigned char param1;
+	unsigned char param2;	// Two parameters, to store some information that is entity-specific
+	signed int wherefrom;
+} baddies[MAX_ENEMIES_ON_SCREEN];
+
+
+
+unsigned char indexToDelete [MAX_ENEMIES_ON_SCREEN];
+//--------------------------------------------------------------
+
+unsigned char temp1 = 0;
+unsigned char temp2 = 0;
+unsigned char temp3 = 0;
+
+
+
+unsigned char enemies_per_level = 0;
+
+//-----------------------------------------------------------------------
+
+unsigned int pointerAddy;
+unsigned int keypress;
+
+unsigned int arrayInput[5];
+
+
+int player_x_position = 0;
+int playerLow = 0;
+int playerHigh = 0;
+int playerStart = 0;
+
+unsigned char enemyToDelete = 254;
---
+++ main.c
@@ -0,0 +1,100 @@
+
+// compile with 
+// zcc +zx -m -create-app -startup=1 -clib=new  main.c -o output
+
+//--------------------- MACROS
+#define memory_bpeek(a)    (*(unsigned char *)(a))
+
+#define MAX_ENEMIES 255
+#define MAX_ENEMIES_ON_SCREEN 6
+//MAX_ENEMIES_ON_SCREEN should be the number of enemies you want to see on screen + 1
+#define NEAR_PLAYER 2
+
+#define UNSIGNED_CHAR_MAX 255
+//--------------------- MACROS
+
+#include <stdio.h>//standard input output
+#include <stdlib.h> // for atoi
+#include <string.h>
+
+
+#include <input.h>
+#include <arch/spectrum.h>
+
+#include <intrinsic.h>//temp to place labels	powerful troubleshooting tool
+
+#include "globalVariables.h"
+#include "functions.h"
+
+
+void main (void)
+{
+	zx_cls(INK_BLACK | PAPER_WHITE);
+	puts("\x16\x01\x02");
+	//		\x16 == set cursor position
+	//		\x01 = x position (01) in hex
+	//		\x02 = y position (0C) in hex
+		
+	printf("Welcome to the most boring game ever\n");
+		
+	player_start_location();		
+	player_x_position = playerStart;
+		
+	//look at enemy_locations[]
+	fill_enemy_location_array (); // this is large array filled in with level info
+	//print_enemy_location_array();
+		
+	while (1)
+	{
+		printf("Player currently at = %d\n", player_x_position);
+		//search_near_to_player(); //sets playerLow and playerHigh
+		fill_baddies_array();
+		print_baddies_array();
+
+		enter_enemy_to_delete();
+		index_cleared_baddies();
+		clean_baddies_array();
+		
+		
+		clean_enemy_array();
+		
+		
+		//print_baddies_array();
+			
+
+			
+		printf("\nPress ENTER for next\n1 for the entire enemy array\n2 for the baddies array");
+			
+		while (1)
+		{
+			if (in_key_pressed( IN_KEY_SCANCODE_1 ))
+			{
+				//printf("\n WTF");
+				print_entire_enemy_array();
+				break;
+			}			
+			if (in_key_pressed( IN_KEY_SCANCODE_2 ))
+			{
+				//printf("\n WTF");
+				print_baddies_array();
+				break;
+			}
+			if (in_key_pressed( IN_KEY_SCANCODE_ENTER ))
+			{
+				break;
+			}
+			in_pause(100);
+				
+		}
+		player_x_position ++;
+		//if (player_x_position > 255)
+		//	break;
+			
+		pauseWipe();
+	}
+
+		//pauseWipe();
+}
+
+
+
